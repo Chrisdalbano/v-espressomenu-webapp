5dae3e6258d272b16d606bdd9d745cf6797cd253
Desmin <chrisdalbano12@gmail.com>
2022-11-12 23:09:59 -0500

    added NewDrink component for drink creation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
 
 import App from "./App.vue";
+
 import drinkInfo from "./components/drinkInfo.vue";
+
+import NewDrink from "./components/NewDrink.vue";
 
 const app = createApp(App);
 app.component("drink-info", drinkInfo);
+app.component("new-drink", NewDrink);
 app.mount("#app");
